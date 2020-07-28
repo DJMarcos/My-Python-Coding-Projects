23f526e79769f2de309209c9746a57a507554f89
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2020-07-28 14:01:53 -0700

    Add files via upload

--- test8.py
+++ test8.py
@@ -8,23 +8,26 @@ with conn:
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_assingment( \
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
         col_file TEXT)")
+
     conn.commit()
 
 conn.close()
 
-    fileList = ('information.docx', 'hello.txt', 'myimage.png', \
+fileList = ('information.docx', 'hello.txt', 'myimage.png', \
                 'mymovie.mpg','world.txt','data.pdf','myphoto.jpg')
 
 
 conn = sqlite3.connect('test10.db')
 
 
+
+
 with conn:
 
     cur = conn.cursor()
-    for each file in fileList:
+    for file in fileList:
         if file.endswith(".txt"):
-            execute statement to insert file into the table
+            cur.execute ("INSERT FILE INTO THE TABLE")
     
 conn.close()
 
@@ -32,11 +35,9 @@ conn.close()
 
 conn = sqlite3.connect('test10.db')
 
-
-
 with conn:
     cur = conn.cursor()
-    cur.execute("SELECT * FROM tbl_assingment")
+    cur.execute("SELECT * FROM tbl_assingment");
     varPerson = cur.fetchall()
     for item in varPerson:
         msg = "text file; {}\ntext file2: {}".format(item[0],item[1])
