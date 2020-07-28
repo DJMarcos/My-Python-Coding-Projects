1b56e4c0acc44d5ab8ad74ce82765d38c2b1f43b
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2020-07-27 17:34:38 -0700

    Add files via upload

--- test8.py
+++ test8.py
@@ -1,45 +1,43 @@
 import sqlite3
 
 
-conn = sqlite3. connect('test.db')
+conn = sqlite3. connect('test10.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_persons( \
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_assingment( \
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_fname TEXT, \
-        col_lname Text, \
-        col_email TEXT \
-        )")
+        col_file TEXT)")
     conn.commit()
 
 conn.close()
 
+    fileList = ('information.docx', 'hello.txt', 'myimage.png', \
+                'mymovie.mpg','world.txt','data.pdf','myphoto.jpg')
 
-conn = sqlite3.connect('test.db')
+
+conn = sqlite3.connect('test10.db')
 
 
 with conn:
+
     cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
-                ('bob','smith','basmith@gmail.comyu'))
-    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
-                ('bob','smith','byumith@gmail.com'))
-    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
-                ('bob','smith','basmith@gmail.com'))
-    conn.commit()
+    for each file in fileList:
+        if file.endswith(".txt"):
+            execute statement to insert file into the table
+    
 conn.close()
 
 
 
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('test10.db')
 
 
 
 with conn:
     cur = conn.cursor()
-    cur.execute("SELECT col_fname,col_lname,col_email FROM tbl_persons WHERE col_fname = 'bob'")
+    cur.execute("SELECT * FROM tbl_assingment")
     varPerson = cur.fetchall()
     for item in varPerson:
-        msg = "First Name; {}\nLast Name: {}\nEmail: {}".format(item[0],item[1],item[2])
+        msg = "text file; {}\ntext file2: {}".format(item[0],item[1])
     print(msg)
